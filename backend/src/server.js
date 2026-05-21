878a869b86ca09bb3aca0273365af70349c06538
Francis Omondi <francisomondi17@gmail.com>
2026-05-21 20:07:09 +0300

    Auth routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,9 @@
 import express from "express";
+import authRoutes from "./routes/auth.routes.js"
 
 const app = express()
 
-app.get("/", (req, res) => {
-    res.send("Hello World")
-})  
+app.use("/api/auth", authRoutes)  
 
 app.listen (3000, () => {
     console.log("Server is running on port 3000")
