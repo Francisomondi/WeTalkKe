a8c235bf3f1702223c8ff0cca569dd027a25610d
Francis Omondi <francisomondi17@gmail.com>
2026-05-23 14:57:48 +0300

    check auth correction

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -143,12 +143,8 @@ export const logout = (req, res) => {
 }   
 
 export const checkAuth = async (req, res) => {
-    try {
-        const user = await userModel.findById(req.user)  
-        if (!user) {
-            return res.status(404).json({message: "User not found"})
-        }
-        res.status(200).json(user)
+    try {       
+        res.status(200).json(req.user)
     } catch (error) {
         console.log("Check Auth Error")
         res.status(500).json({message: "Error checking authentication"})
--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -9,10 +9,7 @@ export const protectRoute = async (req, res, next) => {
         }
         const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        if (!decoded) {
-            return res.status(401).json({message: "Unauthorized"})
-        }
-        const user = await userModel.findById(decoded.userId).select("-password")  
+        const user = await userModel.findById(decoded.id).select("-password")  
         if (!user) {
             return res.status(401).json({message: "Unauthorized"})
         }
