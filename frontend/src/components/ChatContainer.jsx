ab7585926da357ee22eb99ad78fa2b734dc65310
Francis Omondi <francisomondi17@gmail.com>
2026-06-09 23:32:15 +0300

    receive message in real time

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useChatStore } from '../store/useChatStore'
 import ChatHeader from './ChatHeader'
 import MessageInput from './MessageInput'
@@ -9,6 +9,7 @@ const ChatContainer = () => {
 
  const {messages,getMessages,isMessagesLoading,selectedUser,subscribeToNewMessages,unsubscribeFromNewMessages} = useChatStore()
  const {authUser} = useAuthStore()
+ const messageEndRef = useRef(null)
 
  useEffect(()=> {
    if (!selectedUser?._id) return;
@@ -19,6 +20,12 @@ const ChatContainer = () => {
     unsubscribeFromNewMessages()
   }
  },[selectedUser,getMessages,unsubscribeFromNewMessages,subscribeToNewMessages])
+
+ useEffect(() => {
+    if (messageEndRef.current && messages) {
+      messageEndRef.current.scrollIntoView({ behavior: "smooth" })
+    }
+  }, [messages])
 
  if(isMessagesLoading ) return  (
       <div className='flex-1 flex flex-col overflow-auto '>
@@ -36,7 +43,10 @@ const ChatContainer = () => {
         {messages.map((message) => (
           <div 
             key={message._id}
-            className={`chat ${message.senderId === authUser._id ? 'chat-end' : 'chat-start'}`}>
+            className={`chat ${message.senderId === authUser._id ? 'chat-end' : 'chat-start'}`}
+            ref={messageEndRef}
+          >
+              
               <div className= "chat-image avatar">
                 <div className='size-10 rounded-full border border-gray-300'>
                   <img
