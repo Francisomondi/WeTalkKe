01ef71094a72650fbbc6f32a0bddc63be4786e8f
Francis Omondi <francisomondi17@gmail.com>
2026-06-09 22:22:30 +0300

    implementing online users

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -12,11 +12,22 @@ const io = new Server(server, {
     }
 });
 
+const userSocketMap = {};
+
 io.on("connection", (socket) => {
     console.log("A user connected: " , socket.id);
+    const userId = socket.handshake.query.userId;
+    if (userId) {
+        userSocketMap[userId] = socket.id;
+        console.log("User " + userId + " connected with socket ID: " + socket.id);
+
+    }
+    io.emit("online-users", Object.keys(userSocketMap));
     
     socket.on("disconnect", () => {
         console.log("A user disconnected: " , socket.id);
+        delete userSocketMap[userId];
+        io.emit("online-users", Object.keys(userSocketMap));
     });
     
 
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -94,9 +94,14 @@ export const useAuthStore = create((set,get) => ({
     connectSocket: () => {
         const {authUser} = get();
         if(!authUser || get().socket?.connected) return;
-        const socket = io(BASE_URL)
+        const socket = io(BASE_URL,{
+            query: {userId: authUser._id}
+        })
         socket.connect()
         set({socket})
+            socket.on("online-users", (users) => {
+                set({onlineUsers: users})
+            })
     },
 
     disconnectSocket: () => {
