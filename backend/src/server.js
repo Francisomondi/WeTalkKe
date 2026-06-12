5d6fd62007aabf406fe8bbb59a56d99a18febbc1
Francis Omondi <francisomondi17@gmail.com>
2026-06-12 10:11:46 +0300

    final configuration and push

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,11 +8,11 @@ import cors from "cors"
 import cookieparser from "cookie-parser"
 import {server, app} from "./lib/socket.js"
 
+import path from "path"
+
 dotenv.config()
 
-
-console.log(process.env.CLOUDINARY_API_KEY);
-
+const __dirname = path.resolve()
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieparser())
@@ -24,6 +24,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes) 
 app.use("/api/messages", messageRoutes) 
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")))
+
+    app.get("*", (req, res) => {    
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 
 const PORT = process.env.PORT || 3000
